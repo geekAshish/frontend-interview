5724ce34629bc9d438fb0f4889fff012583a01e9
Ashish Kushwaha <ashish@wishfy.club>
2025-04-05 22:13:59 +0530

    password generator

--- 4. Password Generator/src/App.tsx
+++ 4. Password Generator/src/App.tsx
@@ -1,35 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./styles.css";
+import { useState } from "react";
+import usePasswordGenerator from "./hooks/use-password-generator";
+import PasswordStrengthIndicator from "./components/StrengthChecker";
+import Button from "./components/Button";
+import Checkbox from "./components/Checkbox";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [length, setLength] = useState(4);
+  const [checkboxData, setCheckboxData] = useState([
+    { title: "Include Uppercase Letters", state: false },
+    { title: "Include Lowercase Letters", state: false },
+    { title: "Include Numbers", state: false },
+    { title: "Include Symbols", state: false },
+  ]);
+  const [copied, setCopied] = useState(false);
+
+  const handleCheckboxChange = (i) => {
+    const updatedCheckboxData = [...checkboxData];
+    updatedCheckboxData[i].state = !updatedCheckboxData[i].state;
+    setCheckboxData(updatedCheckboxData);
+  };
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(password);
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+  };
+
+  const { password, errorMessage, generatePassword } = usePasswordGenerator();
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      {/* Password Text and Copy */}
+      {password && (
+        <div className="header">
+          <div className="title">{password}</div>
+          <Button
+            text={copied ? "Copied" : "copy"}
+            onClick={handleCopy}
+            customClass="copyBtn"
+          />
+        </div>
+      )}
+      {/* Character Length */}
+      <div className="charlength">
+        <span>
+          <label>Character Length</label>
+          <label>{length}</label>
+        </span>
+        <input
+          type="range"
+          min="4"
+          max="20"
+          value={length}
+          onChange={(e) => setLength(e.target.value)}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      {/* Checkboxes */}
+      <div className="checkboxes">
+        {checkboxData.map((checkbox, index) => {
+          return (
+            <Checkbox
+              key={index}
+              title={checkbox.title}
+              onChange={() => handleCheckboxChange(index)}
+              state={checkbox.state}
+            />
+          );
+        })}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      {/* Strength */}
+      <PasswordStrengthIndicator password={password} />
 
-export default App
+      {/* Error Handling */}
+      {errorMessage && <div className="errorMessage">{errorMessage}</div>}
+
+      {/* Generate Button */}
+      <Button
+        text="Generate Password"
+        onClick={() => generatePassword(checkboxData, length)}
+        customClass="generateBtn"
+      />
+    </div>
+  );
+}
